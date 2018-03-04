ab68640cbb0fa19b2de52ae9c8919bf8d9cda2c5
bryan garzon <bgarzon@hugeinc.com>
2018-03-04 16:22:44 -0500

    adding webpack and eslint libs

---
+++ src/js/bootstrap.jsx
@@ -0,0 +1,14 @@
+/* eslint react/prop-types: 0 */
+import React from 'react';
+import { render } from 'react-dom';
+
+
+const App = () => (
+  <div className='App'>
+    <div className='landing'>
+      <h1>svideo</h1>
+    </div>
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const { resolve } = require("path");
-const webpack = require("webpack");
+const { resolve } = require('path');
+const webpack = require('webpack'); // eslint-disable-line
 
 module.exports = env => {
-  const regStyle = /\.(css|less|styl|scss|sass|sss)$/;
+  // const regStyle = /\.(css|less|styl|scss|sass|sss)$/;
   if (env) {
-    console.log(env);
+    console.log(env); // eslint-disable-line
   }
 
   return {
-    context: resolve("src"),
+    context: resolve('src'),
     entry: {
-      app: "./js/bootstrap.js"
+      app: './js/bootstrap.jsx'
     },
-    devtool: "cheap-eval-source-map",
+    devtool: 'cheap-eval-source-map',
     output: {
-      filename: "bundle.js",
-      path: resolve("public"),
-      publicPath: "/public/"
+      filename: 'bundle.js',
+      path: resolve('public'),
+      publicPath: '/public/'
     },
     stats: {
       colors: true,
@@ -24,29 +24,36 @@ module.exports = env => {
       chunks: true,
       errors: true
     },
+    devServer: {
+      contentBase: resolve('public'),
+      compress: true,
+      port: 3000
+      // TODO https://www.akadia.com/services/ssh_test_certificate.html
+      // generate selfsign certificate to use https and http2
+    },
     module: {
       rules: [
-        // {
-        //   enforce: 'pre',
-        //   test: /\.jsx?$/,
-        //   loader: 'eslint-loader',
-        //   exclude: /node_modules/
-        // },
         {
+          enforce: 'pre',
           test: /\.jsx?$/,
-          loader: "babel-loader",
-          include: resolve("js")
+          loader: 'eslint-loader',
+          exclude: /node_modules/
         },
         {
-          test: regStyle,
-          rules: [
-            {
-              // Apply PostCSS plugins including autoprefixer
-              loader: "postcss-loader",
-              options: { config: { path: "./build-scripts/postcss.config.js" } }
-            }
-          ]
-        }
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          include: resolve('src/js')
+        },
+        // {
+        //   test: regStyle,
+        //   rules: [
+        //     {
+        //       // Apply PostCSS plugins including autoprefixer
+        //       loader: 'postcss-loader',
+        //       options: { config: { path: './build-scripts/postcss.config.js' } }
+        //     }
+        //   ]
+        // }
       ]
     }
   };
