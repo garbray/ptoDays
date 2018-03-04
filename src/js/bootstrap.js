bd4f0d920870592a8ff415870e5392bd8c1a0d5a
bryan garzon <bgarzon@hugeinc.com>
2018-03-04 14:37:31 -0500

    adding eslint and prettier

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,7 +1,13 @@
-class test {
+function console(...params) {
+  console.log(...params); // eslint-disable-line
+}
+
+class Test {
   constructor(initialText) {
-    // console.log(`initial component ${initialText}`);
+    console(
+      `initial component initial component initial component initial component ${initialText}`
+    );
   }
 }
 
-const newTest = new test("ishhhh");
+Test("ishhhh");
